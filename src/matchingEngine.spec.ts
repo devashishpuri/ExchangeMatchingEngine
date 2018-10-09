2c05a69447f2238a15239e93f0e876c6c0e0fd28
Devashish Puri <devashishpuri@gmail.com>
2018-10-09 06:58:40 +0530

    Add Test suite and Auto Compile for VSCode

---
+++ src/matchingEngine.spec.ts
@@ -0,0 +1,16 @@
+import { MatchingEngine, OrderSide } from './matchingEngine';
+
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+const expect = chai.expect;
+
+const matchEngine = new MatchingEngine();
+
+describe('Matching Engine Test Suite', () => {
+
+    it('should place an Order', () => {
+        const order = matchEngine.newOrder('DevD', 23.4, 5, OrderSide.buy);
+        expect(order.order.side).to.equal(OrderSide.buy);
+    });
+
+});
